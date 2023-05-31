f184415867251513d744fdbffc3bebfa7a941ee4
Joe <joe.is.ohara@gmail.com>
2023-05-31 10:07:10 +0100

    Add product id generation strategy

--- ProductService/src/main/java/com/johara/product/model/Product.java
+++ ProductService/src/main/java/com/johara/product/model/Product.java
@@ -3,13 +3,15 @@ package com.johara.product.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Product {
 
     private @Id
-    @GeneratedValue Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 
     private String name;
     private String description;
